f32375e4ba85ea8b312f909dd01a5ec182ec41f4
karthickb2210 <116095490+karthickb2210@users.noreply.github.com>
2024-02-08 10:57:24 +0530

    Add files via upload

---
+++ src/pages/Home.js
@@ -0,0 +1,110 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Link, useParams } from "react-router-dom";
+
+export default function Home() {
+  const [users, setUsers] = useState([]);
+
+  const { id } = useParams();
+ 
+  useEffect(() => {
+    loadUsers();
+    
+  }, []);
+
+  const loadUsers = async () => {
+    
+    const result = await axios.get("http://localhost:8080/users");
+    
+    setUsers(result.data);
+    
+  };
+ 
+  
+  const deleteUser = async (id) => {
+    let pass = prompt("Enter the password :")
+    if(pass === "abc"){
+      await axios.delete(`http://localhost:8080/user/${id}`);
+      loadUsers();
+    }else{
+      alert("Wrong password")
+    }
+    
+  };
+  function findTime(s1,s2){
+    if(s2===""){
+      return s2;
+    }
+    s1 =  s1.split(':');
+
+
+  let totalSeconds1 = parseInt(s1[0] * 3600 + s1[1] * 60 + s1[2]);
+  let totalSeconds2 = parseInt(s2 * 3600 + s2 * 60 + s2)
+    if(totalSeconds1>totalSeconds2){
+      return "Not Met";
+    } 
+    return "met"
+    
+    
+  }
+  return (
+    <div className="container">
+      <div className="py-4">
+        <table className="table border shadow">
+          <thead>
+            <tr>
+              <th scope="col">S.N</th>
+              <th scope="col">Name</th>
+              <th scope="col">Log In Time</th>
+              <th scope="col">Email</th>
+              <th scope="col">Logout</th>
+              <th scope="col">Work Time</th>
+              <th scope="col">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user, index) => (
+              <tr>
+                <th scope="row" key={index}>
+                  {index + 1}
+                </th>
+                <td>{user.name}</td>
+                <td>{user.inTime}</td>
+                <td>{user.email}</td>
+                <td>{user.logOut}</td>
+                <td>{findTime(user.inTime,8)}</td>
+                <td>
+                  <Link
+                    className="btn btn-primary mx-2"
+                    to={`/viewuser/${user.id}`}
+                  >
+                    View
+                  </Link>
+                  {/* <Link
+                    className="btn btn-outline-primary mx-2"
+                    to={`/edituser/${user.id}`}
+                  >
+                    Edit
+                  </Link> */}
+                  <button
+                    className="btn btn-danger mx-2"
+                    onClick={() => deleteUser(user.id)}
+                  >
+                    Delete
+                  </button>
+                  <Link to={`/logoutuser/${user.id}`}>
+                  <button
+                    className="btn btn-danger mx-2"
+                  >
+                    Logout
+                  </button>
+                  </Link>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
---
+++ src/users/AddUser.js
@@ -0,0 +1,105 @@
+import axios from "axios";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+export default function AddUser() {
+  let navigate = useNavigate();
+
+  const [user, setUser] = useState({
+    id:"",
+    name: "",
+    inTime: "",
+    logOut:"",
+    email: "",
+  });
+  
+  let { id ,name,inTime,logOut, email } = user;
+  
+
+  const onInputChange = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value  });
+    // setUser(inTime = currtime());
+  };
+  function currtime(){
+    let d = new Date();
+    return d.toLocaleTimeString('it-IT');
+  }
+
+  const onSubmit = async (e) => {
+    console.log(currtime());
+    
+    e.preventDefault();
+    setUser(user.inTime= currtime())
+    await axios.post("http://localhost:8080/user", user);
+    navigate("/");
+  };
+
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
+          <h2 className="text-center m-4">Register User</h2>
+
+          <form onSubmit={(e) => onSubmit(e)}>
+            <div className="mb-3">
+              <label htmlFor="Name" className="form-label">
+                Id
+              </label>
+              <input
+                type={"text"}
+                className="form-control"
+                placeholder="Enter your id"
+                name="id"
+                value={id}
+                onChange={(e) => onInputChange(e)}
+              />
+               <label htmlFor="Name" className="form-label">
+                Name
+              </label>
+              <input
+                type={"text"}
+                className="form-control"
+                placeholder="Enter your name"
+                name="name"
+                value={name}
+                onChange={(e) => onInputChange(e)}
+              />
+            </div>
+            {/* <div className="mb-3">
+              <label htmlFor="Username" className="form-label">
+                Intime
+              </label>
+              <input
+                type={"text"}
+                className="form-control"
+                placeholder="Enter your LoginTime"
+                name="inTime"
+                value={inTime}
+                onChange={(e) => onInputChange(e)}
+              />
+            </div> */}
+            <div className="mb-3">
+              <label htmlFor="Email" className="form-label">
+                E-mail
+              </label>
+              <input
+                type={"text"}
+                className="form-control"
+                placeholder="Enter your e-mail address"
+                name="email"
+                value={email}
+                onChange={(e) => onInputChange(e)}
+              />
+            </div>
+            <button type="submit" className="btn btn-outline-primary">
+              Submit
+            </button>
+            <Link className="btn btn-outline-danger mx-2" to="/">
+              Cancel
+            </Link>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
